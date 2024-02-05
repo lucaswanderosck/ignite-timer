d8cbe500b7a1e4dac531e24876aa4751835e498e
Lucas Wanderosck <lucaskleinwanderosck@gmail.com>
2024-02-05 14:59:06 -0300

    Atualiza importações de módulos e corrigir erros de formatação

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "./styles/global";
-import { defaultTheme } from "./styles/themes/default";
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export const App: React.FC = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
     </ThemeProvider>
-  );
-};
+  )
+}
